dda9db38f63c2b03abcce1d00ba79a55be081a24
frobi <frobi.webdev@outlook.com>
2025-01-08 13:45:48 +1030

    Call PokeAPI and display 10 of Ash's gen 1 pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react'
 
 export default function Card({
-    existingPokemon,
+    pokemon,
 }) {
 
     return (
         <>
-
+            <div>
+                {pokemon.length > 0 && (
+                    pokemon.map((pokemon, index) => (
+                        <div key={index}>{pokemon.name}</div>
+                    ))
+                )}
+            </div>
         </>
     )
 }
--- src/components/Memory-Cards.jsx
+++ src/components/Memory-Cards.jsx
@@ -1,12 +1,38 @@
 import { useEffect, useState } from 'react'
+import Card from './Card';
 
 export default function MemoryCards() {
-    const [existingPokemon, setExistingPokemon] = useState([]);
+    const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchPokemon() {
+            try {
+                const pokemonIds = [25, 12, 17, 6, 99, 89, 128, 57, 131, 93];
+                const promises = pokemonIds.map(id => fetch(`https://pokeapi.co/api/v2/pokemon/${id}`));
+                const responses = await Promise.all(promises);
+                for (const response of responses) {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                }
+                const data = await Promise.all(responses.map(response => response.json()));
+                setPokemon(data)
+                setLoading(false)
+            } catch (error) {
+                console.error("There's been a problem with your fetch:", error);
+                setError(error.message)
+                setLoading(false)
+            }
+        }
+        fetchPokemon();
+    }, []);
 
     return (
         <>
             <Card
-                existingPokemon={existingPokemon}
+                pokemon={pokemon}
             />
         </>
     )
